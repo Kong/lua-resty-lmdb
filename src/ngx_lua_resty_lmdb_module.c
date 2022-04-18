eed16ebc7a19e7cdcb65f2fcf45f7eef4347d79c
Andrew Shi <andrew.shi@konghq.com>
2022-04-18 20:24:56 +0800

    newP

--- src/ngx_lua_resty_lmdb_module.c
+++ src/ngx_lua_resty_lmdb_module.c
@@ -2,9 +2,202 @@
 #include <ngx_lua_resty_lmdb_module.h>
 #include <openssl/bio.h>
 #include <openssl/evp.h>
-#include <string.h>
+
 #include <memory.h>
 #include <sys/param.h>
+#include "../lmdb/libraries/liblmdb/lmdb.h"
+#include "../lmdb/libraries/liblmdb/module.h"
+
+#include "lmdb.h"
+#include "module.h"
+
+
+
+
+MDB_crypto_hooks MDB_crypto;
+
+static EVP_CIPHER *cipher;
+
+static int mcf_str2key(const char *passwd, MDB_val *key)
+{
+	unsigned int size;
+	EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+	EVP_DigestInit_ex(mdctx, EVP_sha256(), NULL);
+	EVP_DigestUpdate(mdctx, "Just a Constant", sizeof("Just a Constant"));
+	EVP_DigestUpdate(mdctx, passwd, strlen(passwd));
+	EVP_DigestFinal_ex(mdctx, key->mv_data, &size);
+	EVP_MD_CTX_free(mdctx);
+	return 0;
+}
+
+/* cheats - internal OpenSSL 1.1 structures */
+typedef struct evp_cipher_ctx_st {
+    const EVP_CIPHER *cipher;
+    ENGINE *engine;             /* functional reference if 'cipher' is
+                                 * ENGINE-provided */
+    int encrypt;                /* encrypt or decrypt */
+    int buf_len;                /* number we have left */
+    unsigned char oiv[EVP_MAX_IV_LENGTH]; /* original iv */
+    unsigned char iv[EVP_MAX_IV_LENGTH]; /* working iv */
+    unsigned char buf[EVP_MAX_BLOCK_LENGTH]; /* saved partial block */
+    int num;                    /* used by cfb/ofb/ctr mode */
+    /* FIXME: Should this even exist? It appears unused */
+    void *app_data;             /* application stuff */
+    int key_len;                /* May change for variable length cipher */
+    unsigned long flags;        /* Various flags */
+    void *cipher_data;          /* per EVP data */
+    int final_used;
+    int block_mask;
+    unsigned char final[EVP_MAX_BLOCK_LENGTH]; /* possible final block */
+} EVP_CIPHER_CTX;
+
+#define	CHACHA_KEY_SIZE	32
+#define CHACHA_CTR_SIZE	16
+#define CHACHA_BLK_SIZE	64
+#define POLY1305_BLOCK_SIZE	16
+
+typedef struct {
+    union {
+        double align;   /* this ensures even sizeof(EVP_CHACHA_KEY)%8==0 */
+        unsigned int d[CHACHA_KEY_SIZE / 4];
+    } key;
+    unsigned int  counter[CHACHA_CTR_SIZE / 4];
+    unsigned char buf[CHACHA_BLK_SIZE];
+    unsigned int  partial_len;
+} EVP_CHACHA_KEY;
+
+typedef struct {
+    EVP_CHACHA_KEY key;
+    unsigned int nonce[12/4];
+    unsigned char tag[POLY1305_BLOCK_SIZE];
+    unsigned char tls_aad[POLY1305_BLOCK_SIZE];
+    struct { uint64_t aad, text; } len;
+    int aad, mac_inited, tag_len, nonce_len;
+    size_t tls_payload_length;
+} EVP_CHACHA_AEAD_CTX;
+
+static int mcf_encfunc(const MDB_val *src, MDB_val *dst, const MDB_val *key, int encdec)
+{
+	unsigned char iv[12];
+	int ivl, outl, rc;
+	mdb_size_t *ptr;
+	EVP_CIPHER_CTX ctx = {0};
+	EVP_CHACHA_AEAD_CTX cactx;
+
+	ctx.cipher_data = &cactx;
+	ptr = key[1].mv_data;
+	ivl = ptr[0] & 0xffffffff;
+	memcpy(iv, &ivl, 4);
+	memcpy(iv+4, ptr+1, sizeof(mdb_size_t));
+	EVP_CipherInit_ex(&ctx, cipher, NULL, key[0].mv_data, iv, encdec);
+	EVP_CIPHER_CTX_set_padding(&ctx, 0);
+	if (!encdec) {
+		EVP_CIPHER_CTX_ctrl(&ctx, EVP_CTRL_AEAD_SET_TAG, key[2].mv_size, key[2].mv_data);
+	}
+	rc = EVP_CipherUpdate(&ctx, dst->mv_data, &outl, src->mv_data, src->mv_size);
+	if (rc)
+		rc = EVP_CipherFinal_ex(&ctx, key[2].mv_data, &outl);
+	if (rc && encdec) {
+		EVP_CIPHER_CTX_ctrl(&ctx, EVP_CTRL_AEAD_GET_TAG, key[2].mv_size, key[2].mv_data);
+	}
+	return rc == 0;
+}
+
+static const MDB_crypto_funcs mcf_table = {
+	mcf_str2key,
+	mcf_encfunc,
+	NULL,
+	CHACHA_KEY_SIZE,
+	POLY1305_BLOCK_SIZE,
+	0
+};
+
+MDB_crypto_funcs *MDB_crypto()
+{
+	cipher = (EVP_CIPHER *)EVP_chacha20_poly1305();
+	return (MDB_crypto_funcs *)&mcf_table;
+}
+
+
+
+
+// void *mlm_load(const char *file, const char *name, MDB_crypto_funcs **mcf_ptr, char **errmsg)
+// {
+// 	MDB_crypto_hooks *hookfunc;
+// 	void *ret = NULL;
+// 	if (!name)
+// 		name = "MDB_crypto";
+
+// #ifdef _WIN32
+// 	{
+// 		HINSTANCE mlm = LoadLibrary(file);
+// 		if (mlm) {
+// 			hookfunc = GetProcAddress(mlm, name);
+// 			if (hookfunc)
+// 				*mcf_ptr = hookfunc();
+// 			else {
+// 				*errmsg = "Crypto hook function not found";
+// 				FreeLibrary(mlm);
+// 				mlm = NULL;
+// 			}
+// 		} else {
+// 			*errmsg = GetLastError();
+// 		}
+// 		ret = (void *)mlm;
+// 	}
+// #else
+// 	{
+// 		void *mlm = dlopen(file, RTLD_NOW);
+// 		if (mlm) {
+// 			hookfunc = dlsym(mlm, name);
+// 			if (hookfunc)
+// 				*mcf_ptr = hookfunc();
+// 			else {
+// 				*errmsg = "Crypto hook function not found";
+// 				dlclose(mlm);
+// 				mlm = NULL;
+// 			}
+// 		} else {
+// 			*errmsg = dlerror();
+// 		}
+// 		ret = mlm;
+// 	}
+// #endif
+// 	return ret;
+// }
+
+// void mlm_unload(void *mlm)
+// {
+// #ifdef _WIN32
+// 	FreeLibrary((HINSTANCE)mlm);
+// #else
+// 	dlclose(mlm);
+// #endif
+// }
+
+// void *mlm_setup(MDB_env *env, const char *file, const char *password, char **errmsg)
+// {
+// 	MDB_crypto_funcs *cf;
+// 	MDB_val enckey = {0};
+// 	void *mlm = mlm_load(file, NULL, &cf, errmsg);
+// 	if (mlm) {
+// 		if (cf->mcf_sumfunc) {
+// 			mdb_env_set_checksum(env, cf->mcf_sumfunc, cf->mcf_sumsize);
+// 		}
+// 		if (cf->mcf_encfunc && password) {
+// 			char keybuf[2048];
+// 			enckey.mv_data = keybuf;
+// 			enckey.mv_size = cf->mcf_keysize;
+// 			if (cf->mcf_str2key)
+// 				cf->mcf_str2key(password, &enckey);
+// 			else
+// 				strncpy(enckey.mv_data, password, enckey.mv_size);
+// 			mdb_env_set_encrypt(env, cf->mcf_encfunc, &enckey, cf->mcf_esumsize);
+// 			memset(enckey.mv_data, 0, enckey.mv_size);
+// 		}
+// 	}
+// 	return mlm;
+// }
 
 
 static void *ngx_lua_resty_lmdb_create_conf(ngx_cycle_t *cycle);
@@ -70,235 +263,63 @@ ngx_module_t  ngx_lua_resty_lmdb_module = {
     NGX_MODULE_V1_PADDING
 };
 
-// chacha 8 enc
-// static int qencfunc(const MDB_val *src, MDB_val *dst, const MDB_val *key, int encdec)
+
+// --me_encfunc(&in, &out, enckeys, 0)
+// static int encfunc(const MDB_val *src, MDB_val *dst, const MDB_val *key, int encdec)
 // {
-// 	chacha8(src->mv_data, src->mv_size, key[0].mv_data, key[1].mv_data, dst->mv_data);
-// 	return 0;
+//     unsigned char iv[12];
+// 	int ivl, outl, rc;
+// 	mdb_size_t *ptr;
+// 	EVP_CIPHER_CTX ctx = {0};
+// 	EVP_CHACHA_AEAD_CTX cactx;
+
+// 	ctx.cipher_data = &cactx;
+// 	ptr = key[1].mv_data;
+// 	ivl = ptr[0] & 0xffffffff;
+// 	memcpy(iv, &ivl, 4);
+// 	memcpy(iv+4, ptr+1, sizeof(mdb_size_t));
+// 	EVP_CipherInit_ex(&ctx, cipher, NULL, key[0].mv_data, iv, encdec);
+// 	EVP_CIPHER_CTX_set_padding(&ctx, 0);
+// 	if (!encdec) {
+// 		EVP_CIPHER_CTX_ctrl(&ctx, EVP_CTRL_AEAD_SET_TAG, key[2].mv_size, key[2].mv_data);
+// 	}
+// 	rc = EVP_CipherUpdate(&ctx, dst->mv_data, &outl, src->mv_data, src->mv_size);
+// 	rc = EVP_CipherFinal_ex(&ctx, key[2].mv_data, &outl);
+// 	if (encdec) {
+// 		EVP_CIPHER_CTX_ctrl(&ctx, EVP_CTRL_AEAD_GET_TAG, key[2].mv_size, key[2].mv_data);
+// 	}
+// 	return rc == 0;
 // }
 
-int DesEncode(char *inbuf, char *outbuf, int len)
-{
-	int ret = 0;
 
-	ret = EVP_EncodeBlock(outbuf, inbuf, len);
-	if(ret > 0)
-	{
-		printf("====base64_encode_outLen====%ld\n", strlen(outbuf));
-		printf("%s\n\n", outbuf);
-		return 0;
-	}
-	
-	return 1;
-}
+// char password[] = "This is my passphrase for now...";
+// void *mlm;
+// char *errmsg;
 
 
-int DesDecode(char *inbuf, char *outbuf, int *len)
-{
-	int ret = 0;
-
-	ret = EVP_DecodeBlock(outbuf, inbuf, *len);
-	if (ret > 0)
-	{
-		*len = ret - ret%4;
-		printf("====base64_decode_outbuf====%s\n,len==:%d\n", outbuf, ret);
-		return 0;
-	}
-	
-	return 1;	
-}
-
-int DesEncrypt(const unsigned char *key,
-								   char *inbuf, int inlen,
-								   char *outbuf, int *outlen)
-{		
-	EVP_CIPHER_CTX *ctx = NULL;
-	ctx = EVP_CIPHER_CTX_new();
-	EVP_EncryptInit_ex(ctx, EVP_aes_256_ecb(), NULL, key, NULL);
- 
-	if (!EVP_EncryptUpdate(ctx, outbuf, outlen, inbuf, inlen))
-	{
-		EVP_CIPHER_CTX_free(ctx);
-		return 1;
-	}
-	
-	int tmplen = 0;
-	if (!EVP_EncryptFinal_ex(ctx, outbuf + *outlen, &tmplen))
-	{
-		EVP_CIPHER_CTX_free(ctx);
-		return 1;
-	}
- 
-	*outlen += tmplen;
-	EVP_CIPHER_CTX_free(ctx);
-	printf("====encrype_outbuf====%s\n,len==:%d\n", outbuf, *outlen);
-
-	return 0;
-}
-
-int DesDecrypt(const unsigned char *key,
-								   char *inbuf, int *inlen,
-								   char *outbuf, int *outlen)
-{
-	EVP_CIPHER_CTX *ctx = NULL;
-	
-	ctx = EVP_CIPHER_CTX_new();
-	EVP_DecryptInit_ex(ctx, EVP_aes_256_ecb(), NULL, key, NULL);
- 
-	if (!EVP_DecryptUpdate(ctx, outbuf, outlen, inbuf, *inlen))
-	{
-		return 1;
-	}
-
-	int tmplen = 0;
-	
-	if (!EVP_DecryptFinal_ex(ctx, outbuf + *outlen, &tmplen))
-	{
-		EVP_CIPHER_CTX_free(ctx);
-		return 1;
-	}
-	*outlen += tmplen;
-	//
-	EVP_CIPHER_CTX_free(ctx);
-	outbuf[*outlen] = '\0';
-	printf("====decode_outbuf====%s\n,len==:%d\n", outbuf, *outlen);
-	return 0;
-}
-
-int abupEncryAndEncode(const unsigned char *key, char *inbuf, int inlen,
-								   char *outbuf, int *outlen)
-{
-	int ret = 0;
-	int out1len = 0;
-
-	char *out1buf = malloc(10024);		
-	if(out1buf)
-	{
-		memset(out1buf, 0x00, 10024);
-	}
-	ret = DesEncrypt(key, inbuf, inlen, out1buf, &out1len);
-	if(ret)
-	{
-		return 1;
-	}
-	ret = DesEncode(out1buf, outbuf, out1len);
-    free(out1buf);
-    out1buf = NULL;
-	if(ret)
-	{
-		return 1;
-	}
-	*outlen = strlen(outbuf);
-	printf("==encrypt_encode_ok==\n");
-	return 0;
-}
-
-int abupDecryAndDecode(const unsigned char *key,
-								   char *inbuf, int *inlen,
-								   char *outbuf, int *outlen)
-{
-	int ret = 0;
-	
-	char *out1buf = malloc(10024);
-	if(out1buf)
-	{
-		memset(out1buf, 0x00, 10024);
-	}
-	
-	ret = DesDecode(inbuf, out1buf, inlen);//
-	if(ret)
-	{
-		return 1;
-	}
-	printf("==decode_ok==%d\n",*inlen);
-	
-	ret = DesDecrypt(key, out1buf, inlen, outbuf, outlen);
-    free(out1buf);
-    out1buf = NULL;
-	if(ret)
-	{
-		return 1;
-	}
-	return 0;
-}
-
-//--me_encfunc(&in, &out, enckeys, 0)
-int encfunc(const MDB_val *src, MDB_val *dst, const MDB_val *key, int encdec)
-{
-	char *key_t = key->mv_data;         
-	char *inbuf = src->mv_data;
-
-	printf("============================================================================\n");		
-	printf("-------key_t-----%s         ",key_t);
-    printf("          -----------------\n");
-
-    printf("============================================================================\n");		
-	printf("-------src->mv_data-----%s         ",src->mv_data);
-    printf("        -----------------\n");
-
-//-----------chacha8(src->mv_data,      src->mv_size,   key[0].mv_data,         key[1].mv_data,     dst->mv_data);
-//-----------chacha8(const void* data,  size_t length,  const uint8_t* key,     const uint8_t* iv,  char* cipher) 
-
-
-	char *outbuf = malloc(10024);
-	if(outbuf)
-	{
-		memset(outbuf, 0x00, 10024);
-	}
-	
-	char *tmpbuf = malloc(10024);
-	if(tmpbuf)
-	{
-		memset(tmpbuf, 0x00, 10024);
-	}
-	int outLen = 0;int tmpLen = 0; 	
-
-	printf("============================================================================\n");		
-	printf("-------inbuf-----%s         ",inbuf);
-    printf("        -----------------\n");	
-	if(encdec == 1)
-    {
-        abupEncryAndEncode(key_t,inbuf, strlen(inbuf), outbuf, &outLen);
-        dst->mv_data = outbuf;
-        return 0;
-    }
-    else if(encdec == 0)
-    {
-        abupDecryAndDecode(key_t,outbuf,&outLen,tmpbuf,&tmpLen);
-        
-        // printf("====inbuf=%s,inLen=%ld\n",inbuf,strlen(inbuf));
-
-
-        // DesEncode(inbuf, outbuf, strlen(inbuf));
-
-        // DesEncrypt(key_t, inbuf, strlen(inbuf), outbuf, &outLen);
-
-        // DesDecrypt(key_t, outbuf, &outLen, tmpbuf, &tmpLen);
-
-        // DesDecode(outbuf, tmpbuf, &tmpLen);
-
-        dst->mv_data = tmpbuf;
-        printf("============================================================================\n");
-        //printf("--------------%s------------\n",data,"\n------------------------");
-        printf("----dst->data-------------%s         ",dst->mv_data);
-        printf("                 -----------------\n");	
-        printf("----src->data-------------%s         ",src->mv_data);
-        printf("                 -----------------\n");
-
-        // free(outbuf);
-        // outbuf = NULL;
-        // free(tmpbuf);
-        // tmpbuf = NULL;
-
-        return 0;
-    }
-
-    else
-    {
-        printf("no encdec");
-        return 1;
-    }
-}
+// void *mlm_setup(MDB_env *env, const char *file, const char *password, char **errmsg)
+// {
+// 	MDB_crypto_funcs *cf;
+// 	MDB_val enckey = {0};
+// 	void *mlm = mlm_load(file, NULL, &cf, errmsg);
+// 	if (mlm) {
+// 		if (cf->mcf_sumfunc) {
+// 			mdb_env_set_checksum(env, cf->mcf_sumfunc, cf->mcf_sumsize);
+// 		}
+// 		if (cf->mcf_encfunc && password) {
+// 			char keybuf[2048];
+// 			enckey.mv_data = keybuf;
+// 			enckey.mv_size = cf->mcf_keysize;
+// 			if (cf->mcf_str2key)
+// 				cf->mcf_str2key(password, &enckey);
+// 			else
+// 				strncpy(enckey.mv_data, password, enckey.mv_size);
+// 			mdb_env_set_encrypt(env, cf->mcf_encfunc, &enckey, cf->mcf_esumsize);
+// 			memset(enckey.mv_data, 0, enckey.mv_size);
+// 		}
+// 	}
+// 	return mlm;
+// }
 
 static void *
 ngx_lua_resty_lmdb_create_conf(ngx_cycle_t *cycle)
@@ -367,6 +388,12 @@ static ngx_int_t ngx_lua_resty_lmdb_init_worker(ngx_cycle_t *cycle)
         return NGX_ERROR;
     }
 
+	// mlm = mlm_setup(lcf->env, "./crypto.lm", password, &errmsg);
+	// if (!mlm) {
+	// 	fprintf(stderr,"Failed to load crypto module: %s\n", errmsg);
+	// 	exit(1);
+	// }
+
     rc = mdb_env_set_mapsize(lcf->env, lcf->map_size);
     if (rc != 0) {
         ngx_log_error(NGX_LOG_CRIT, cycle->log, 0,
@@ -382,12 +409,17 @@ static ngx_int_t ngx_lua_resty_lmdb_init_worker(ngx_cycle_t *cycle)
     }
 
 // test use
-    MDB_val enckey;
+    MDB_val enckey[3];
+    MDB_crypto_funcs *cf;
     char ekey[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
 		17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32};
-    enckey.mv_data = ekey;
-	enckey.mv_size = sizeof(ekey);
-    rc = mdb_env_set_encrypt(lcf->env, encfunc, &enckey, 0);
+    enckey[0].mv_data = ekey;
+	enckey[0].mv_size = sizeof(ekey);
+    enckey[1].mv_data = ekey;
+	enckey[1].mv_size = sizeof(ekey);
+    enckey[1].mv_data = ekey;
+	enckey[2].mv_size = sizeof(ekey);
+    rc = mdb_env_set_encrypt(lcf->env, cf->mcf_encfunc, &enckey, cf->mcf_esumsize);
     if (rc != 0) {
         ngx_log_error(NGX_LOG_CRIT, cycle->log, 0, "unable to set LMDB encryption key: %s", mdb_strerror(rc));
         return NGX_ERROR;
