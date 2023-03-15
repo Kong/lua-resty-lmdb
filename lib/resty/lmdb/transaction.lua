b12dfe033d9b6a0b7517c7ad56084f4d6721673c
chronolaw <chrono_cpp@me.com>
2023-03-15 19:00:49 +0800

    normalize large key

--- lib/resty/lmdb/transaction.lua
+++ lib/resty/lmdb/transaction.lua
@@ -71,6 +71,23 @@ function _M.begin(hint)
 end
 
 
+local normalize_key
+do
+    local resty_sha256 = require("resty.sha256")
+
+    -- lmdb has 511 bytes limit for key
+    local MAX_KEY_SIZE = 511
+
+    normalize_key = function(key)
+        if key and #key > MAX_KEY_SIZE then
+            return resty_sha256(key)
+        end
+
+        return key
+    end
+end
+
+
 local get_dbi
 do
     local CACHED_TXN_DBI = _M.begin(1)
@@ -120,7 +137,7 @@ end
 function _TXN_MT:get(key, db)
     local op = self:_new_op()
     op.opcode = "GET"
-    op.key = key
+    op.key = normalize_key(key)
     op.db = db or DEFAULT_DB
 end
 
@@ -128,7 +145,7 @@ end
 function _TXN_MT:set(key, value, db)
     local op = self:_new_op()
     op.opcode = "SET"
-    op.key = key
+    op.key = normalize_key(key)
     op.value = value
     op.db = db or DEFAULT_DB
     op.flags = 0
